43251b9e804c293988b71350f1418486efa3fe9f
sidestepism <fushimi.ryohe.i@gmail.com>
2014-10-15 13:20:32 +0900

    first commit

---
+++ siglunch.js
@@ -0,0 +1,48 @@
+var gpio = require("pi-gpio");
+
+var interval = null;
+
+var Yo = require("yo-api");
+yo_lunch = new Yo("bb3233f2-a2f3-4870-bdee-2c99cb9c6401");
+yo_coffee = new Yo("0ea30d1d-d00e-4aff-9eae-e36d3085a9f4");
+
+
+
+function start(){
+interval = setInterval(function(){
+    gpio.read(7, function(err, value) {
+        if(value){
+            console.log("lunch");
+            sendYo("lunch");
+            sendTweet("lunch");
+            stop();
+            setTimeout(start, 5000);
+        }
+    });
+    gpio.read(11, functio(err, value){
+        if(value){
+            console.log("coffee");
+            sendYo("coffee");
+            sendTweet("coffee");
+            stop();
+            setTimeout(start, 5000);
+    })
+}, 100);
+}
+
+function stop(){
+    if(interval) clearInterval(interval);
+    interval = null;
+}
+
+function sendYo(msg){
+    if(msg == "coffee"){
+        yo_coffee.yo_all();
+    }else if(msg == "lunch"){
+        yo_lunch.yo_all();
+    }
+}
+
+function sendTweet(msg){
+    
+}
